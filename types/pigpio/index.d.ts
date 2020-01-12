ffcea4d6d6dd19da5afa71a11d7ef854f03f983e
Roger Schönbächler <schoero@gmx.ch>
2020-01-12 23:34:22 +0100

    Added myself to Defintions by

--- types/pigpio/index.d.ts
+++ types/pigpio/index.d.ts
@@ -3,6 +3,7 @@
 // Definitions by: ManerFan <https://github.com/manerfan>
 //                 erikma <https://github.com/erikma>
 //                 park012241 <https://github.com/park012241>
+//                 Rogerrrrrrrs <https://github.com/Rogerrrrrrrs>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="node" />
